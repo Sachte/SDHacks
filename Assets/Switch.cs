4d03ef2bc61a104b52f98b2a8650cefd7825c85a
Sachte <vijaysachet@yahoo.com>
2016-10-01 22:12:11 -0700

    aa

--- Assets/Switch.cs
+++ Assets/Switch.cs
@@ -1,19 +1,22 @@
-﻿namespace VRTK.Examples
+﻿namespace VRTK.examples
 {
     using UnityEngine;
     using System.Collections;
+    using UnityEngine.UI;
 
     public class Switch : VRTK_InteractableObject
     {
-        enum Toggle { ON, OFF };
-        Toggle buttonStatus;
         Vector3 local_rot;
+        public GameObject TooltipPrefab;
+        public string name;
+        public GameObject tootip;
+        protected bool isDisplayed;
 
         protected override void Start()
         {
-            buttonStatus = Toggle.OFF;
             base.Start();
             local_rot = this.transform.eulerAngles;
+            isDisplayed = false;
         }
 
         protected override void Update()
@@ -24,14 +27,50 @@
         public override void StartTouching(GameObject currentTouchingObject)
         {
             base.StartTouching(currentTouchingObject);
-            Debug.Log((object) "its touching");
-            if (buttonStatus == Toggle.OFF) {
-                Debug.Log((object) "aasdfasdf");
-                this.transform.Rotate(Vector3.right, 47.495f, Space.Self);
-            } else {
-                Debug.Log((object)"asdfasdfasdf");
-                this.transform.Rotate(Vector3.right, -47.495f , Space.Self);
+            /*  Debug.Log((object) "its touching");
+              if (isOpen) {
+                  Debug.Log((object) "aasdfasdf");
+                  this.transform.Rotate(Vector3.right, 47.495f, Space.Self);
+                  Vector3 close = this.transform.position;
+                  this.transform.Translate(close.x + 0f, close.y - 1.70955f, close.z - 0.7199778f, Space.Self);
+                  isOpen = false;
+              } else {
+                  Debug.Log((object)"asdfasdfasdf");
+                  this.transform.Rotate(Vector3.right, -47.495f , Space.Self);
+                  Vector3 open = this.transform.position;
+                  this.transform.Translate(open.x + 0f, open.y + 1.70955f, open.z + 0.7199778f, Space.Self);
+                  isOpen = t rue;*/
+            //}
+            //VRTK_ObjectTooltip tip = new VRTK_ObjectTooltip();
+            //tip.displayText = name;
+            if (!isDisplayed)
+            {
+                
+                tootip = GameObject.Instantiate<GameObject>(TooltipPrefab);
+                isDisplayed = true;
+
+                tootip.GetComponent<VRTK_ObjectTooltip>().displayText = name;
+                tootip.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+                Vector3 temp = this.transform.position;
+                tootip.transform.position = new Vector3(0f, 1f, 0f) + temp;
+
+                tootip.GetComponent<VRTK_ObjectTooltip>().drawLineFrom = this.transform;
+                tootip.GetComponent<VRTK_ObjectTooltip>().drawLineTo = tootip.transform;
+
             }
+            // transform position rotation
+            // set drawline transforms
+            // set text
+            // change rotation to face the head.
+        }
+
+        public override void StopTouching(GameObject previousTouchingObject)
+        {
+            base.StopTouching(previousTouchingObject);
+
+            tootip.GetComponent<VRTK_ObjectTooltip>().enabled = false;
+
+            isDisplayed = false;
         }
 
     }
---
+++ Assets/Tooltips.cs
@@ -0,0 +1,110 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Tooltips : VRTK_ObjectTooltip
+    {
+        /*
+        public string name;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // *****************************************************************
+        [Tooltip("The text that is displayed on the tooltip.")]
+        public string displayText;
+        [Tooltip("The size of the text that is displayed.")]
+        public int fontSize = 14;
+        [Tooltip("The size of the tooltip container where `x = width` and `y = height`.")]
+        public Vector2 containerSize = new Vector2(0.1f, 0.03f);
+        [Tooltip("An optional transform of where to start drawing the line from. If one is not provided the centre of the tooltip is used for the initial line position.")]
+        public Transform drawLineFrom;
+        [Tooltip("A transform of another object in the scene that a line will be drawn from the tooltip to, this helps denote what the tooltip is in relation to. If no transform is provided and the tooltip is a child of another object, then the parent object's transform will be used as this destination position.")]
+        public Transform drawLineTo;
+        [Tooltip("The width of the line drawn between the tooltip and the destination transform.")]
+        public float lineWidth = 0.001f;
+        [Tooltip("The colour to use for the text on the tooltip.")]
+        public Color fontColor = Color.black;
+        [Tooltip("The colour to use for the background container of the tooltip.")]
+        public Color containerColor = Color.black;
+        [Tooltip("The colour to use for the line drawn between the tooltip and the destination transform.")]
+        public Color lineColor = Color.black;
+
+        private LineRenderer line;
+
+        /// <summary>
+        /// The Reset method resets the tooltip back to its initial state
+        /// </summary>
+        public void Reset()
+        {
+            SetContainer();
+            SetText("UITextFront");
+            SetText("UITextReverse");
+            SetLine();
+            if (drawLineTo == null && transform.parent != null)
+            {
+                drawLineTo = transform.parent;
+            }
+        }
+
+        private void Start()
+        {
+            Reset();
+        }
+
+        private void SetContainer()
+        {
+            transform.FindChild("TooltipCanvas").GetComponent<RectTransform>().sizeDelta = containerSize;
+            var tmpContainer = transform.FindChild("TooltipCanvas/UIContainer");
+            tmpContainer.GetComponent<RectTransform>().sizeDelta = containerSize;
+            tmpContainer.GetComponent<Image>().color = containerColor;
+        }
+
+        private void SetText(string name)
+        {
+            var tmpText = transform.FindChild("TooltipCanvas/" + name).GetComponent<Text>();
+            tmpText.material = Resources.Load("UIText") as Material;
+            tmpText.text = displayText.Replace("\\n", "\n");
+            tmpText.color = fontColor;
+            tmpText.fontSize = fontSize;
+        }
+
+        private void SetLine()
+        {
+            line = transform.FindChild("Line").GetComponent<LineRenderer>();
+            line.material = Resources.Load("TooltipLine") as Material;
+            line.material.color = lineColor;
+            line.SetColors(lineColor, lineColor);
+            line.SetWidth(lineWidth, lineWidth);
+            if (drawLineFrom == null)
+            {
+                drawLineFrom = transform;
+            }
+        }
+
+        private void DrawLine()
+        {
+            if (drawLineTo)
+            {
+                line.SetPosition(0, drawLineFrom.position);
+                line.SetPosition(1, drawLineTo.position);
+            }
+        }
+
+        private void Update()
+        {
+            DrawLine();
+        }*/
+
+        // *****************************************************************
+    }
+}
