c4043b9d7d8ccc2af68bb4e532ae54d1abc46305
Sachte <vijaysachet@yahoo.com>
2016-10-02 05:31:27 -0700

    Second to final commit

--- Assets/Switch.cs
+++ Assets/Switch.cs
@@ -11,12 +11,14 @@
         public string name;
         public GameObject tootip;
         protected bool isDisplayed;
+        static bool nonePresent;
 
         protected override void Start()
         {
             base.Start();
             local_rot = this.transform.eulerAngles;
             isDisplayed = false;
+            nonePresent = true;
         }
 
         protected override void Update()
@@ -27,51 +29,45 @@
         public override void StartTouching(GameObject currentTouchingObject)
         {
             base.StartTouching(currentTouchingObject);
-            /*  Debug.Log((object) "its touching");
-              if (isOpen) {
-                  Debug.Log((object) "aasdfasdf");
-                  this.transform.Rotate(Vector3.right, 47.495f, Space.Self);
-                  Vector3 close = this.transform.position;
-                  this.transform.Translate(close.x + 0f, close.y - 1.70955f, close.z - 0.7199778f, Space.Self);
-                  isOpen = false;
-              } else {
-                  Debug.Log((object)"asdfasdfasdf");
-                  this.transform.Rotate(Vector3.right, -47.495f , Space.Self);
-                  Vector3 open = this.transform.position;
-                  this.transform.Translate(open.x + 0f, open.y + 1.70955f, open.z + 0.7199778f, Space.Self);
-                  isOpen = t rue;*/
-            //}
-            //VRTK_ObjectTooltip tip = new VRTK_ObjectTooltip();
-            //tip.displayText = name;
-            if (!isDisplayed)
-            {
-                
-                tootip = GameObject.Instantiate<GameObject>(TooltipPrefab);
-                isDisplayed = true;
 
-                tootip.GetComponent<VRTK_ObjectTooltip>().displayText = name;
-                tootip.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
-
-                tootip.transform.position = new Vector3(1.7f, 2.5f, -3.1f);
-              
-                tootip.GetComponent<VRTK_ObjectTooltip>().drawLineFrom = this.transform;
-                tootip.GetComponent<VRTK_ObjectTooltip>().drawLineTo = tootip.transform;
-
-            }
-            // transform position rotation
-            // set drawline transforms
-            // set text
-            // change rotation to face the head.
+            StartCoroutine(Stall());
         }
 
         public override void StopTouching(GameObject previousTouchingObject)
         {
             base.StopTouching(previousTouchingObject);
             
-            Object.Destroy(tootip, 3f);
-    
+            GameObject.Destroy(tootip, 0f);
+            Debug.Log("is Destroyed");
+            
+            nonePresent = true;
             isDisplayed = false;
+
         }
 
+        IEnumerator Stall()
+        {
+            if (!isDisplayed && nonePresent)
+            {
+                isDisplayed = true;
+
+                nonePresent = false;
+
+                tootip = GameObject.Instantiate<GameObject>(TooltipPrefab);
+
+                tootip.GetComponent<VRTK_ObjectTooltip>().displayText = name;
+                tootip.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+
+                tootip.transform.position = new Vector3(1.7f, 2.5f, -3.1f);
+
+                tootip.GetComponent<VRTK_ObjectTooltip>().drawLineFrom = this.transform;
+                tootip.GetComponent<VRTK_ObjectTooltip>().drawLineTo = tootip.transform;
+
+            }
+            yield return new WaitForSeconds(4);
+            
+
+        }
+        
     }
 }
